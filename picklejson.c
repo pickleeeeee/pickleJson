4885ae19b131db366881dd84dddc7604a5495a0d
pickleeeeee <2537422279@qq.com>
2022-11-21 00:14:56 +0800

    tutorial3

--- picklejson.c
+++ picklejson.c
@@ -2,18 +2,21 @@
 // Created by Pickle on 2022/11/19.
 //*******************************
 #include "picklejson.h"
-#include <assert.h>
-#include <stdlib.h>
-#include <math.h>
+#include <assert.h> /* assert() */
+#include <stdlib.h> /* errno, NULL */
+#include <math.h>   /* ERANGE, HUGE_VAL */
+#include <string.h> /* memcpy() */
 
 #define EXPECT(c, ch)    do { assert(*c->json==(ch)); c->json++; } while(0)
 #define ISDIGIT(ch)      ((ch) >= '0' && (ch) <= '9')
 #define ISDIGIT1TO9(ch)      ((ch) >= '1' && (ch) <= '9')
 
-
 typedef struct{
     const char* json;
+    char* stack;
+    size_t size,top;
 }pickle_context;
+
 
 /**
  * 删除字符串之前的空白
@@ -57,8 +60,8 @@ static int pickle_parse_number(pickle_context* c,pickle_value* v){
         for(p++; ISDIGIT(*p); p++);
     }
     errno = 0;
-    v->n = strtod(c->json,NULL);
-    if(errno == ERANGE && (v->n == HUGE_VAL || v->n == -HUGE_VAL))
+    v->u.n = strtod(c->json,NULL);
+    if(errno == ERANGE && (v->u.n == HUGE_VAL || v->u.n == -HUGE_VAL))
         return PICKLE_PARSE_NUMBER_TOO_BIG;
     v->type = PICKLE_NUMBER;
     c->json = p;
@@ -80,8 +83,11 @@ int pickle_parse(pickle_value* v, const char* json){
     int ret = 0;
     assert(v != NULL);
     c.json = json;
+    //初始化栈区
+    c.stack = NULL;
+    c.size = c.top = 0;
     //如果解析失败v将会返回NULL值，函数中先将v设置成了NULL值
-    v->type = PICKLE_NULL;
+    pickle_init(v);
     //移除字符前的空白
     pickle_parse_whitespace(&c);
     //只有再成功解析后才分析是不是又多个root
@@ -90,6 +96,8 @@ int pickle_parse(pickle_value* v, const char* json){
         if(c.json[0]!='\0')
             ret = PICKLE_PARSE_ROOT_NOT_SINGULAR;
     }
+    assert(c.top == 0);
+    free(c.stack);
     return ret;
 }
 
@@ -98,7 +106,62 @@ pickle_type pickle_get_type(const pickle_value* v){
     return v->type;
 }
 
+/**
+ * 释放节点字符数组所占的内存空间
+ * @param v
+ */
+void pickle_free(pickle_value* v){
+    assert(v != NULL);
+    if(v->type == PICKLE_STRING)
+        //如果是string类型，直接释放s指向的字符数组空间
+        free(v->u.s.s);
+    v->type = PICKLE_NULL;
+}
+
+/**
+ * @param v 存储字符的结点
+ * @param s 被保存的字符数组
+ * @param len 字符数组长度
+ */
+void pickle_set_string(pickle_value* v,const char* s, size_t len){
+    assert(v != NULL && (s != NULL || len == 0));
+    //释放字符数组内存
+    pickle_free(v);
+    v->u.s.s = (char*) malloc(len + 1);
+    memcpy(v->u.s.s, s, len);
+    v->u.s.s[len] = '\0';
+    v->u.s.len = len;
+    v->type = PICKLE_STRING;
+}
+
 double pickle_get_number(const pickle_value* v){
     assert(v != NULL && v->type == PICKLE_NUMBER);
-    return v->n;
+    return v->u.n;
+}
+
+#ifndef PICKLE_PARSE_STACK_INIT_SIZE
+#define PICKLE_PARSE_STACK_INTI_SIZE 256
+#endif
+//缓冲区的push操作
+void pickle_context_push(pickle_context* c, size_t size){
+    void* ret;
+    assert(size > 0);
+    //栈空间扩容
+    if(c->top + size >= c->size){
+        //初始化缓冲区大小
+        if(c->size == 0)
+            c->size = PICKLE_PARSE_STACK_INTI_SIZE;
+        while(c->top + size >= c->size)
+            //每次扩容1.5倍
+            c->size += c->size >> 1;
+        c->stack = realloc(c->stack, c->size);
+    }
+    //TODO 没看懂
+    ret = c->stack + c->top;
+    c->top += size;
+    return ret;
+}
+
+void pickle_set_number(pickle_value* v, double n){
+
 }
--- picklejson.h
+++ picklejson.h
@@ -1,10 +1,15 @@
 #ifndef PICKLEJSON_H__
 #define PICKLEJSON_H__
 
+#include <stddef.h> /* size_t */
+
 typedef enum{PICKLE_NULL, PICKLE_FALSE, PICKLE_TRUE, PICKLE_NUMBER, PICKLE_STRING, PICKLE_ARRAY, PICKLE_OBJECT} pickle_type;
 
 typedef struct {
-    double n;
+    union{
+        struct{ char* s; size_t  len;} s;    /* string */
+        double n;                            /* number */
+    } u;
     pickle_type type;
 }pickle_value;
 
@@ -16,11 +21,25 @@ enum {
     PICKLE_PARSE_NUMBER_TOO_BIG
 };
 
+#define pickle_set_null(v) pickle_free(v)
+
+#define pickle_init(v) do { (v)->type = PICKLE_NULL; } while(0)
+
 int pickle_parse(pickle_value* v, const char* json);
 
 pickle_type  pickle_get_type(const pickle_value* v);
 
+void pickle_free(pickle_value* v);
+
+int pickle_get_boolean(const pickle_value* v);
+void pickle_set_boolean(pickle_value* v, int b);
+
+const char* pickle_get_string(const pickle_value* v);
+size_t pickle_get_len(const pickle_value* v);
+void pickle_set_string(pickle_value* v, const char* s, size_t size);
+
 //当且仅当pickle_type是PICKLE_NUMBER的时候,n才表示json的数字的数值
 double pickle_get_number(const pickle_value* v);
+void pickle_set_number(pickle_value* v,double n);
 
 #endif
